a5f410982149b3d5e523625d40f953c064134524
Wei Lyu <lvwei1001@gmail.com>
2026-03-15 21:38:31 +0900

    change Area to method for rectangle and circle, add shape interface

--- structs/shape.go
+++ structs/shape.go
@@ -1,14 +1,29 @@
+// Package structs
 package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
 
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
